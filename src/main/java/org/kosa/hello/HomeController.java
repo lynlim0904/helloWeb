3fe68d003c465b80937995c9d75ea06414ae1c03
lynlim0904 <lynlim0904@gmail.com>
2025-06-11 11:14:02 +0900

    first commit

---
+++ src/main/java/org/kosa/hello/HomeController.java
@@ -0,0 +1,17 @@
+package org.kosa.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.ServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class HomeController {
+	@GetMapping("/")
+	public String index(ServletRequest request) {
+		log.info("server port = {}", request.getServerPort());
+		return "Hello World... " + request.getServerPort();
+	}
+}
